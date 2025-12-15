9e4f73d57f53255c65b2422cd1fd54b7b2f314db
EMZgm <andy.rafael2005@gmail.com>
2025-12-14 23:56:08 -0500

    cambiando

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
